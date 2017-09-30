869912e5a733ee0ab8341e4aa39549026f3d7331
Sean Kao <seankao31@gmail.com>
2017-09-30 17:11:53 +0800

    Avg: 21952, estimate value of player play 2 moves

--- heuristic.h
+++ heuristic.h
@@ -18,7 +18,7 @@ public:
         score = b.move(opcode);
         empty = 0;
         value = 0;
-        max_index = 0;
+        int max_value = 0;
         /**
          *
          * coef{0, 1, 2, 3, 4, 5, 6}
@@ -28,26 +28,50 @@ public:
          *  (3)  (4)  (5)  (6)
          *
          */
-        int max_value = 0;
-        for (int pos = 0; pos < 4; pos++) {
-            value = 0;
-            board b_rotate = b;
-            b_rotate.rotate(pos);
-            for (int r = 0; r < 4; r++) {
-                for (int c = 0; c < 4; c++) {
-                    value += coef[r+c] * b_rotate[r][c];
+        // this block consider one additional move
+        // simply assume player can move 2 times in a row
+        for (int op = 0; op < 4; op++) {
+            board b_move = b;
+            int score2 = b_move.move(op);
+            for (int rt = 0; rt < 4; rt++) {
+                value = 0;
+                board b_rotate = b_move;
+                b_rotate.rotate(rt);
+                for (int r = 0; r < 4; r++) {
+                    for (int c = 0; c < 4; c++) {
+                        value += coef[r+c] * b_rotate[r][c];
+                        if(b_rotate[r][c] == 0)
+                            empty++;
+                    }
                 }
+                value += 10 * score2;
+                value += 15 * empty;
+                max_value = std::max(max_value, value);
             }
-            max_value = std::max(max_value, value);
         }
         value = max_value;
-        for (int i = 0; i < 16; i++) {
-            if (b(i) == 0)
-                empty++;
-        }
-        max_tile = i2t(max_index);
-        value += 10 * score;
-        value += 15 * empty;
+
+        // --- this block consider only this next move
+        // for (int rt = 0; rt < 4; rt++) {
+        //     value = 0;
+        //     board b_rotate = b;
+        //     b_rotate.rotate(rt);
+        //     for (int r = 0; r < 4; r++) {
+        //         for (int c = 0; c < 4; c++) {
+        //             value += coef[r+c] * b_rotate[r][c];
+        //         }
+        //     }
+        //     max_value = std::max(max_value, value);
+        // }
+        // for (int i = 0; i < 16; i++) {
+        //     if (b(i) == 0)
+        //         empty++;
+        // }
+        // value = max_value;
+        // value += 10 * score;
+        // value += 15 * empty;
+        // ---
+
         value = (score == -1)? score: value;
         // std::cout << value << std::endl;
         // std::cout << std::endl << value << std::endl << b;
@@ -61,6 +85,4 @@ private:
     int value; // final heuristic value
     int empty; // number of empty tiles
     int score; // score gained by this move
-    int max_index;
-    int max_tile;
 };
